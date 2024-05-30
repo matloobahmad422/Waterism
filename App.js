4901818633d88b12c32efea6ceb1735ee16e9f37
matloobahmad422 <matloobahmad422@gmail.com>
2024-05-30 03:07:38 -0700

    datepicker template modal

--- App.js
+++ App.js
@@ -2,5 +2,5 @@ import BurgerPepsi from "./src/components/BurgerPepsi";
 import BurgerWebView from "./src/components/BurgerWebView";
 import Home from "./src/screens/Home";
 export default function App() {
-  return <BurgerWebView />;
+  return <Home />;
 }
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,11 @@
-import { FlatList, View, Text } from "react-native";
+import { FlatList, View } from "react-native";
 import BurgerPepsi from "../components/BurgerPepsi";
+import DatePickerModal from "../components/DatePickerModal";
 export default function Home() {
   const BurgerPepsiCountData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   const _renderItem = ({ Item }) => <BurgerPepsi />;
   return (
-    <View>
+    <View className="flex">
       <FlatList
         numColumns={3}
         data={BurgerPepsiCountData}
@@ -15,6 +16,7 @@ export default function Home() {
           marginTop: 100,
         }}
       />
+      <DatePickerModal />
     </View>
   );
 }
